eb7820f9249f859bbf2cc0c2e23a2385c638cbe7
Nick Kamer <nick@ziaomedia.com>
2022-09-17 20:56:54 +0200

    feat: added axios + react-query

---
+++ src/components/providers/providers.tsx
@@ -0,0 +1,11 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { FC, PropsWithChildren } from "react";
+import { queryClient } from "../../lib/app/queryClient";
+
+/**
+ * As we are often left with a ton of providers wrapping your app, I like to abstract them all out into one component to
+ * keep things readable
+ */
+export const Providers: FC<PropsWithChildren> = ({ children }) => {
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { Router } from "./components/app/router/router";
+import { Providers } from "./components/providers/providers";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>Hello world</React.StrictMode>
+  <React.StrictMode>
+    <Providers>
+      <RouterProvider router={Router} />
+    </Providers>
+  </React.StrictMode>
 );
